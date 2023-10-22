9c418962ce1b7e29112a20437b48d7d92bb4fa6b
Kippie <jenstheminer@gmail.com>
2023-10-22 11:31:26 +0200

    Fixed bossbar and tablist

--- LunarisMC-Plugin/src/main/java/dev/kippenboutske/listeners/joinListener.java
+++ LunarisMC-Plugin/src/main/java/dev/kippenboutske/listeners/joinListener.java
@@ -1,7 +1,7 @@
 package dev.kippenboutske.listeners;
 
 import net.kyori.adventure.bossbar.BossBar;
-import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
@@ -13,15 +13,15 @@ import java.awt.*;
 public class joinListener implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        final Component name = (Component) net.kyori.adventure.text.Component.text("Welcome in LunarisMC!");
-        final BossBar bossbar = BossBar.bossBar((ComponentLike) name, 1, BossBar.Color.BLUE, BossBar.Overlay.NOTCHED_20);
+        final Component name = net.kyori.adventure.text.Component.text("Awesome BossBar");
+        final BossBar bossbar = BossBar.bossBar( name, 1, BossBar.Color.BLUE, BossBar.Overlay.NOTCHED_20);
         e.getPlayer().showBossBar(bossbar);
         e.getPlayer().sendMessage("Hello " + e.getPlayer().getDisplayName() + "! Welcome in LunarisMC !");
         e.getPlayer().sendMessage("Your current ping is: " + e.getPlayer().getPing() + "MS!");
         e.setJoinMessage(ChatColor.GRAY + "[ų] " + e.getPlayer().getDisplayName());
         final Component header = (Component) net.kyori.adventure.text.Component.text("My Cool Server", NamedTextColor.BLUE);
         final Component footer = (Component) net.kyori.adventure.text.Component.text("Have a great time!" + "/n" + "Cheese");
-        e.getPlayer().sendPlayerListHeaderAndFooter((ComponentLike) header, (ComponentLike) footer);
+        e.getPlayer().sendPlayerListHeaderAndFooter( header, footer);
 
     }
 }

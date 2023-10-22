baccce992b8f3f7aa6e151c497e678a38569a49b
Kippie <jenstheminer@gmail.com>
2023-10-22 11:21:25 +0200

    LALALA

---
+++ LunarisMC-Plugin/src/main/java/dev/kippenboutske/listeners/joinListener.java
@@ -0,0 +1,4 @@
+package dev.kippenboutske.listeners;
+
+public class joinListener {
+}
--- LunarisMC-Plugin/src/main/java/dev/kippenboutske/lunarismcplugin.java
+++ LunarisMC-Plugin/src/main/java/dev/kippenboutske/lunarismcplugin.java
@@ -1,13 +1,19 @@
 package dev.kippenboutske;
 
+import dev.kippenboutske.listeners.joinListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class lunarismcplugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getLogger().info("██╗░░░░░██╗░░░██╗███╗░░██╗░█████╗░██████╗░██╗░██████╗███╗░░░███╗░█████╗░");
+        Bukkit.getLogger().info("██║░░░░░██║░░░██║████╗░██║██╔══██╗██╔══██╗██║██╔════╝████╗░████║██╔══██╗");
+        Bukkit.getLogger().info("██║░░░░░██║░░░██║██╔██╗██║███████║██████╔╝██║╚█████╗░██╔████╔██║██║░░╚═╝");
+        Bukkit.getLogger().info("██║░░░░░██║░░░██║██║╚████║██╔══██║██╔══██╗██║░╚═══██╗██║╚██╔╝██║██║░░██╗");
+        Bukkit.getLogger().info("███████╗╚██████╔╝██║░╚███║██║░░██║██║░░██║██║██████╔╝██║░╚═╝░██║╚█████╔╝");
+        Bukkit.getLogger().info("╚══════╝░╚═════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═════╝░╚═╝░░░░░╚═╝░╚════╝░");
     }
 
     @Override
